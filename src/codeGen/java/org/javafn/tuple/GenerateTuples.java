7844b0690e4dcfbe6c1af5f50068580ac6e5c59e
Ben Stringer <ben.stringer@usm.edu>
2026-04-26 13:54:24 -0500

    Added an IntObjPair to prove I could handle the generics

--- src/codeGen/java/org/javafn/tuple/GenerateTuples.java
+++ src/codeGen/java/org/javafn/tuple/GenerateTuples.java
@@ -15,36 +15,34 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.function.BiConsumer;
-import java.util.function.BiFunction;
-import java.util.function.BiPredicate;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
+
+import static java.util.function.Predicate.not;
 
 public class GenerateTuples {
 
     public static final String PACKAGE_NAME = "org.javafn.tuples";
 
     record Tuple(
-            int numElements,
-            ClassName name,
-            ClassName predicate,
-            ClassName consumer,
-            ClassName mapper
+            List<TypeName> types,
+//            int numElements,
+            ClassName name
+//            ClassName predicate,
+//            ClassName consumer,
+//            ClassName mapper
     ) {
-        public IntStream idx() { return IntStream.range(1, numElements + 1); }
-        public ParameterizedTypeName parameterizedMapper(final List<TypeVariableName> vars, TypeVariableName retType) {
-            final int nArgs = vars.size();
-            final TypeName[] args = new TypeName[nArgs + 1];
-            for (int i = 0; i < nArgs; i++) {
-                args[i] = vars.get(i);
-            }
-            args[nArgs] = retType;
-            return ParameterizedTypeName.get(mapper, args);
-        }
+        public IntStream idx() { return IntStream.range(0, types.size()); }
+//        public ParameterizedTypeName parameterizedMapper(final List<TypeVariableName> vars, TypeVariableName retType) {
+//            final int nArgs = vars.size();
+//            final TypeName[] args = new TypeName[nArgs + 1];
+//            for (int i = 0; i < nArgs; i++) {
+//                args[i] = vars.get(i);
+//            }
+//            args[nArgs] = retType;
+//            return ParameterizedTypeName.get(mapper, args);
+//        }
     }
-
-    record Var(TypeVariableName type, ParameterSpec field) { }
 
     public static void main(String[] args) throws IOException {
         final File out = new File("build/generated/main/java");
@@ -54,65 +52,99 @@ public class GenerateTuples {
             }
         }
 
-        final var pair = new Tuple(2,
-                ClassName.get(PACKAGE_NAME, "PairType"),
-                ClassName.get(BiPredicate.class),
-                ClassName.get(BiConsumer.class),
-                ClassName.get(BiFunction.class));
+        final var pair = new Tuple(
+                List.of(ClassName.OBJECT, ClassName.OBJECT),
+                ClassName.get(PACKAGE_NAME, "PairType"));
+//                ClassName.get(BiPredicate.class),
+//                ClassName.get(BiConsumer.class),
+//                ClassName.get(BiFunction.class));
         genTuple(out, pair);
-
+        genTuple(out, new Tuple(List.of(ClassName.INT, ClassName.OBJECT), ClassName.get(PACKAGE_NAME, "IntObjPair")));
     }
 
     static void genTuple(final File packageDir, final Tuple tuple) throws IOException {
         final List<TypeVariableName> varTypes = tuple.idx()
-                .mapToObj(i -> TypeVariableName.get("V" + i))
+                .mapToObj(i -> {
+                    final var type = tuple.types.get(i);
+                    if (type == ClassName.OBJECT) {
+                        return TypeVariableName.get("V" + (i+1));
+                    } else {
+                        return TypeVariableName.get(type.toString());
+                    }
+                })
                 .toList();
+        final TypeVariableName[] genericArgs = tuple.idx()
+                .filter(i -> !tuple.types.get(i).isPrimitive())
+                .mapToObj(varTypes::get)
+                .toArray(TypeVariableName[]::new);
         final List<ParameterSpec> varFields = tuple.idx()
-                .mapToObj(i -> ParameterSpec.builder(varTypes.get(i-1), "v" + i).build())
+                .mapToObj(i -> ParameterSpec.builder(varTypes.get(i), "v" + (i+1), Modifier.FINAL).build())
                 .toList();
+
         final TypeVariableName zType = TypeVariableName.get("Z");
         final ParameterSpec zField = ParameterSpec.builder(zType, "z").build();
 
         final var tupleClassBuilder = TypeSpec.recordBuilder(tuple.name);
 
+        // Define the record, its generic arguments, and fields
         tupleClassBuilder
-                .addTypeVariables(varTypes)
+                .addTypeVariables(Arrays.asList(genericArgs))
                 .recordConstructor(MethodSpec.constructorBuilder()
-                        .addParameters(varFields)
-                        .build())
+                        .addParameters(tuple.idx().mapToObj(i -> ParameterSpec.builder(varTypes.get(i), "v" + (i+1)).build()).toList())
+                        .build());
+        // Define static factory methods
+        tupleClassBuilder
                 .addMethod(MethodSpec.methodBuilder("of")
-                        .returns(ParameterizedTypeName.get(tuple.name, varTypes.toArray(TypeVariableName[]::new)))
-                        .addTypeVariables(varTypes)
+                        .returns(ParameterizedTypeName.get(tuple.name, genericArgs))
+                        .addTypeVariables(Arrays.asList(genericArgs))
                         .addParameters(varFields)
                         .addModifiers(Modifier.PUBLIC, Modifier.STATIC)
                         .addStatement("return new $L<>($L)", tuple.name.simpleName(),
                                 varFields.stream().map(ParameterSpec::name).collect(Collectors.joining(", ")))
-                        .build())
-                .addMethod(MethodSpec.methodBuilder("map")
-                        .addTypeVariable(zType)
-                        .returns(zType)
-                        .addParameter(ParameterSpec.builder(
-                                tuple.parameterizedMapper(varTypes, zType), "fn", Modifier.FINAL)
-                                .build())
-                        .addModifiers(Modifier.PUBLIC)
-                        .addStatement("return fn.apply($L)", varFields.stream()
-                                .map(ParameterSpec::name)
-                                .collect(Collectors.joining(", ")))
                         .build());
+//        tupleClassBuilder
+//                .addMethod(MethodSpec.methodBuilder("map")
+//                        .addTypeVariable(zType)
+//                        .returns(zType)
+//                        .addParameter(ParameterSpec.builder(
+//                                tuple.parameterizedMapper(varTypes, zType), "fn", Modifier.FINAL)
+//                                .build())
+//                        .addModifiers(Modifier.PUBLIC)
+//                        .addStatement("return fn.apply($L)", varFields.stream()
+//                                .map(ParameterSpec::name)
+//                                .collect(Collectors.joining(", ")))
+//                        .build());
+        // Generate setters for each field.  Primitive types remain fixed, but generic types can change.
         tuple.idx().forEach(i -> {
-            final TypeVariableName[] types = varTypes.toArray(TypeVariableName[]::new);
-            types[i-1] = zType;
-            final List<ParameterSpec> fields = new ArrayList<>(varFields);
-            fields.set(i-1, zField);
-            tupleClassBuilder.addMethod(MethodSpec.methodBuilder("v" + i)
-                    .addTypeVariable(zType)
-                    .returns(ParameterizedTypeName.get(tuple.name, types))
-                    .addParameter(ParameterSpec.builder(zType, "z").addModifiers(Modifier.FINAL).build())
+            final TypeName setterType = tuple.types.get(i);
+            final MethodSpec.Builder methodBuilder = MethodSpec.methodBuilder("v" + (i + 1));
+            final TypeVariableName argType;
+            final TypeVariableName[] genericTypes;
+            if (setterType.isPrimitive()) {
+                argType = TypeVariableName.get(setterType.toString());
+                genericTypes = genericArgs;
+            } else {
+                argType = zType;
+                genericTypes = tuple.idx()
+                        .filter(j -> !tuple.types.get(j).isPrimitive())
+                        .mapToObj(j -> {
+                            if (i == j) return zType;
+                            else return varTypes.get(j);
+                        })
+                        .toArray(TypeVariableName[]::new);
+                methodBuilder.addTypeVariable(zType);
+            }
+            final List<ParameterSpec> argNames = new ArrayList<>(varFields);
+            argNames.set(i, zField);
+            methodBuilder
+                    .returns(ParameterizedTypeName.get(tuple.name, genericTypes))
+                    .addParameter(ParameterSpec.builder(argType, "z").addModifiers(Modifier.FINAL).build())
                     .addModifiers(Modifier.PUBLIC)
                     .addStatement("return $T.of($L)",
                             tuple.name,
-                            fields.stream().map(ParameterSpec::name).collect(Collectors.joining(", ")))
-                    .build());
+                            argNames.stream().map(ParameterSpec::name).collect(Collectors.joining(", ")))
+                    .build();
+            tupleClassBuilder.addMethod(methodBuilder.build());
         });
 
         JavaFile.builder(PACKAGE_NAME, tupleClassBuilder.build())
